4be2b86704f2c4acbbedc417b3bab4e385be3c9a
Allie <Allie>
2016-12-04 23:18:42 -0600

    News, gallery, and courses

--- nocsa/mail/contact_me.php
+++ nocsa/mail/contact_me.php
@@ -14,12 +14,12 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'emailme@myprogrammingblog.com'; // hi mate thanks for purchase guna theme, just replace your email with emailme@myprogrammingblog.com
+$to = 'alliemart7@gmail.com'; // hi mate thanks for purchase guna theme, just replace your email with emailme@myprogrammingblog.com
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message \n $message";
-$headers = "From: contacts@myprogrammingblog.com\n";
+$headers = "From: alliemart7@gmail.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
